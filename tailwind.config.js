c99ec1f4e4c96078cefafc49740b7d894c4e77ea
buihaik58vnua-eng <buihaik58vnua@gmail.com>
2026-05-12 17:21:29 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: { extend: {} },
+  content: [
+    "./index.html",
+    "./App.jsx",
+    "./main.jsx",
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 }
