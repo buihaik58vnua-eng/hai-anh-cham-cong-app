97e0a60614e410a1f834955ab9ecbecd8d610e05
buihaik58vnua-eng <buihaik58vnua@gmail.com>
2026-05-12 17:40:18 +0700

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://YOUR_PROJECT.supabase.co";
-const supabaseKey = "YOUR_ANON_KEY";
+const supabaseUrl = "DÁN_PROJECT_URL";
+const supabaseKey = "DÁN_PUBLISHABLE_KEY";
 
 export const supabase = createClient(
   supabaseUrl,
